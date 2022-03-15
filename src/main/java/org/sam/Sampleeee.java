e444aecc8ee99c88ed7d2a76ab08d304ecf96a04
Gayathri123-4 <gayurey0606@gmail.com>
2022-03-15 11:03:21 +0530

    common1

---
+++ src/main/java/org/sam/Sampleeee.java
@@ -0,0 +1,36 @@
+package org.sam;
+
+import org.testng.annotations.Test;
+
+public class Sampleeee {
+	@Test
+	private void test1() {
+		System.out.println("Sample1");
+
+	}
+	@Test
+	private void test2() {
+		System.out.println("Sample2");
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
